90405dd73e53ac48f18fa1702cc9826bf50b1699
pavitharanath <pavithranath@cloudrevelinnovation.com>
2026-07-24 21:08:43 +0530

    day 04 task completed

--- Day-04/index.js
+++ Day-04/index.js
@@ -75,3 +75,120 @@ switch (name) {
 
 
 //  2. Build an ATM Cash Withdrawal System
+// Rajan goes to the Axis bank ATM. He enters an amount to withdraw. 
+// The ATM only allows multiples of 100. 
+// Print “Withdrawal successful” if valid, otherwise print “Invalid amount”.
+
+let amount = 1050
+
+if (amount % 100 == 0) {
+    console.log("Withdrawal successful", amount % 100);
+
+} else {
+    console.log("Invalid amount", amount % 100);
+
+}
+
+
+// 3. Build a Calculator with switch-case
+
+function calculate(){
+    const aValue = Number(document.getElementById("aValue").value);
+    const bValue = Number(document.getElementById("bValue").value);
+    const operator = document.getElementById("operator").value;
+
+    let result;
+
+    switch(operator){
+        case "+":
+            result = aValue + bValue;
+            break;
+        case "-":
+            result = aValue - bValue;
+            break;
+        case "*":
+            result = aValue * bValue;
+            break;
+        case "/":
+            result = aValue / bValue;
+            break;
+        case "%":
+            result = aValue % bValue;
+            break;
+        default:
+            result = "Invalid operator";
+
+    }
+
+    document.getElementById("result").textContent =   `${aValue} ${operator} ${bValue} = ${result}`;
+
+
+
+
+
+}
+
+
+
+
+// 4. Pay for your movie ticket
+
+
+
+const MovieTicker =(age) => {
+
+    switch (true) {
+        case age <= 18: console.log("Pay $3");
+            break;
+        case (age >= 18 && age <= 60): console.log("Pay $10");
+            break;
+        case age > 60: console.log("Pay $8");
+            break;
+        default : console.log("Not allowed")
+    }
+    
+}
+
+MovieTicker(69)
+
+// or
+
+const MovieTicker2 = (age) => {
+
+    if (age <= 18) return console.log("Pay $3");
+    else if (age >= 18 && age <= 60) return console.log("Pay $10")
+    else if (age > 60) return console.log("Pay $8")
+    else return console.log("Not allowed")
+
+
+}
+
+MovieTicker2(69)
+
+// 6. Which Triangle?
+
+function CheckTriangle(){
+    const triangleA = Number(document.getElementById("triangleA").value);
+    const triangleB = Number(document.getElementById("triangleB").value);
+    const triangleC = Number(document.getElementById("triangleC").value);
+
+    let resultTri;
+
+    if(triangleA == triangleB && triangleB == triangleC){
+         resultTri = "Equilateral Triangle"
+    }
+    else if (triangleA == triangleB ||
+         triangleA == triangleC ||triangleB == triangleC
+    ) {
+         resultTri = "Isosceles Triangle"
+    }
+    else if(triangleA !== triangleB && triangleB !== triangleC){
+            resultTri = "Scalene Triangle"
+    }
+
+    document.getElementById("typeTrina").textContent = `The Triangle is ${resultTri}`
+
+
+
+
+}
