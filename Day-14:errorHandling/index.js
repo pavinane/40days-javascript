29e6f69cc7c06329fd6b03e628b54c5d37d2291c
pavitharanath <pavithranath@cloudrevelinnovation.com>
2026-08-18 00:01:24 +0530

    error handling

--- Day-14:errorHandling/index.js
+++ Day-14:errorHandling/index.js
@@ -91,13 +91,3 @@ function processInformation(information) {
 
 processInformation("Hi pavi")
 
-// 1. What will be the output of the following code?
-// try {
-//     let r = p + 50;
-//     console.log(r);
-// } catch (error) {
-//     console.log("An error occurred:", error.name);
-// }
-
-
-// output  = ReferenceError
